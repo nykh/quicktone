323870746c8ef60a10ed8ed2dfc73ffbb7387468
nykh <nicholas.ykhuang@gmail.com>
2018-05-13 22:15:39 -0700

    Add submit

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,17 @@
       Your browser does not support the <code>audio</code> element.
     </audio>
     <Toner :depth="depth" :breadth="breadth"
-      @next="next_or_submit"/>
+      @next="next_or_submit($event)"/>
+    <ul v-if="errors && errors.length">
+      <li v-for="(error, key) of errors" :key="key">
+        {{error.message}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Toner from './components/Toner';
 import Config from './experiment.json';
 
@@ -24,22 +30,31 @@ export default {
       depth: Config.length,
       breadth: Config.width,
       test_id: 0,
+      results: [],
+      errors: [],
     };
   },
   computed: {
     sound_file() {
-      if (Config.debug) {
-        return require('./assets/001.mp3'); // eslint-disable-line global-require
-      }
-      const num = this.leftpad(String(this.test_id), 3, '0');
+      const num = this.leftpad(String(this.test_id + 1), 3, '0');
 
       // eslint-disable-next-line
       return require(`./assets/${num}.mp3`);
     },
   },
   methods: {
-    next_or_submit() {
-
+    next_or_submit(result) {
+      this.results.push(result);
+      if (this.test_id + 1 >= this.numTests) {
+        this.submit();
+      } else {
+        this.test_id += 1;
+      }
+    },
+    submit() {
+      axios.post(Config.post_url, { body: 1 })
+        .then(() => {})
+        .catch((e) => { this.errors.push(e); });
     },
     leftpad(_str, _len, _ch) {
       let str = String(_str);
