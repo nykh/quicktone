7ccda4d3af3f4eb5ef13c332729baed2217b07a0
nykh <nicholas.ykhuang@gmail.com>
2018-05-20 14:05:53 -0700

    Make ToneView

---
+++ src/components/ToneView.vue
@@ -0,0 +1,78 @@
+<template lang="html">
+  <div class="toneview">
+    <h3>{{title}}</h3>
+    <svg class="canvas"
+      :width="(2 * xorigin) + ((len - 1) * xgap)"
+      :height="(2 * yorigin) + ((ydim - 1) * ygap)">
+      <g v-for="i in range(0, compareLines.length)" :key="'g' + i">
+        <path :d="comparepaths[i]" :stroke="colors[i]" opacity="0.2"/>
+        <circle v-for="j in range(0, len)" :key="'cp' + i + ',' + j"
+                :fill="colors[i]" :r="r" opacity="0.2"
+                :cx="comparepts[i][0][j]" :cy="comparepts[i][1][j]" />
+      </g>
+      <path :d="mainpath" stroke="black"/>
+      <circle v-for="i in range(0, len)" :key="'mp' + i"
+              fill="black" :r="r"
+              :cx="mainpts[0][i]" :cy="mainpts[1][i]" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToneView',
+  props: ['title', 'mainLine', 'compareLines'],
+  created() {
+    this.ydim = 5;
+    this.len = this.mainLine.length;
+    this.xorigin = 10;
+    this.yorigin = 10;
+    this.r = 2;
+    this.xgap = 40;
+    this.ygap = 25;
+    this.colors = ['green', 'red', 'blue'];
+  },
+  computed: {
+    mainpts() {
+      return this.toCoord(this.mainLine);
+    },
+    mainpath() {
+      return this.makePath(this.mainpts);
+    },
+    comparepts() {
+      return this.compareLines.map(line => this.toCoord(line));
+    },
+    comparepaths() {
+      return this.comparepts.map(coords => this.makePath(coords));
+    },
+  },
+  methods: {
+    toCoord(points) {
+      return [this.range(0, points.length).map(x => (x * this.xgap) + this.xorigin),
+        points.map(y => (y * this.ygap) + this.yorigin)];
+    },
+    makePath(coords) {
+      let s = `M${this.xorigin} ${this.yorigin}`;
+      const [xs, ys] = coords;
+      s += ` M${xs[0]} ${ys[0]}`;
+      for (let i = 1; i < coords[0].length; i += 1) {
+        s += ` L${xs[i]} ${ys[i]}`;
+      }
+      return s;
+    },
+    range(start, end) {
+      return Array.from(new Array(end - start), (_, i) => i + start);
+    },
+  },
+};
+</script>
+
+<style lang="css">
+.toneview h3 {
+  margin-bottom: 0;
+}
+
+.toneview .canvas {
+  background-color: white;
+}
+</style>
