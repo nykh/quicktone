305f5c940a2a01e47ef1788c08fdbcc2ebb6495b
nykh <nicholas.ykhuang@gmail.com>
2018-05-20 14:17:20 -0700

    Make code to test toneviews

--- src/components/ThankYou.vue
+++ src/components/ThankYou.vue
@@ -2,14 +2,28 @@
   <div id="app">
     <p>Thank you for participating in this study!</p>
     <p><router-link to="/">Start another test</router-link></p>
+    <p>{{store.results}}</p>
     <p>{{store.response}}</p>
+    <ToneView v-for="i in range(0, store.results.length)"
+      :key="'test' + i" :title="'Test ' + i"
+      :mainLine="store.results[i]" :compareLines="store.reponse[i]"></ToneView>
   </div>
 </template>
 
 <script>
+import ToneView from './ToneView';
+
 export default {
   name: 'ThankYou',
   props: ['store'],
+  components: {
+    ToneView,
+  },
+  methods: {
+    range(start, end) {
+      return Array.from(new Array(end - start), (_, i) => i + start);
+    },
+  },
 };
 </script>
 
